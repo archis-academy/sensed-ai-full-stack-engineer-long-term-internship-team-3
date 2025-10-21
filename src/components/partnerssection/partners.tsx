5b9647261ab8b7eecd7a2c8cc1997a64c829250b
nursahtuncel <nursahtuncell@gmail.com>
2025-10-21 03:24:24 +0300

    [SE-13] - add id for array

--- src/components/partnerssection/partners.tsx
+++ src/components/partnerssection/partners.tsx
@@ -2,44 +2,32 @@ import styles from "./partners.module.scss";
 
 const Partners: React.FC = () => {
   const partners = [
-    {
-      name: "Google",
-      image: "google.png",
-    },
-    {
-      name: "Amazon",
-      image: "amazon.png",
-    },
-    {
-      name: "Logitech",
-      image: "logitech.png",
-    },
-    {
-      name: "Spotify",
-      image: "spotify.png",
-    },
-    {
-      name: "Samsung",
-      image: "samsung.png",
-    },
-    {
-      name: "Netflix",
-      image: "netflix.png",
-    },
+    { name: "Google", image: "google.png", id: 1 },
+    { name: "Amazon", image: "amazon.png", id: 2 },
+    { name: "Logitech", image: "logitech.png", id: 3 },
+    { name: "Spotify", image: "spotify.png", id: 4 },
+    { name: "Samsung", image: "samsung.png", id: 5 },
+    { name: "Netflix", image: "netflix.png", id: 6 },
   ];
+
   return (
     <section className={styles.partners}>
       <div className={styles.bigBlueBlob}>
-        <img src="Ellipse.png" alt="" />
+        <img src="/images/Ellipse.png" alt="Ellipse" />
       </div>
-      <h6>Trusted by 100+ Companies across the globe! </h6>
-      <ul className={styles.logos}>
-     {partners.map((partner) => (
-  <li key={partner.image} className={styles.logo}>
-    <img src={partner.image} alt={partner.name} />
-  </li>
-))}
 
+      <h6>Trusted by 100+ Companies across the globe!</h6>
+
+      <ul className={styles.logos}>
+        {partners.map((partner) => (
+          <li key={partner.id} className={styles.logo}>
+            <img
+              src={`/images/${partner.image}`}
+              alt={`${partner.name} logo`}
+              loading="lazy"
+            />
+          </li>
+        ))}
       </ul>
     </section>
   );
